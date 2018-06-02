8039135b42c903384ab382e4b6b55464f2b37010
knives81 <maurizio.pinzi@gmail.com>
2018-06-02 20:00:41 +0100

    Spring Boot Prj

---
+++ src/main/java/com/dashboard/commondashboard/CommondashboardApplication.java
@@ -0,0 +1,12 @@
+package com.dashboard.commondashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommondashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommondashboardApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dashboard/commondashboard/CommondashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dashboard.commondashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommondashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
